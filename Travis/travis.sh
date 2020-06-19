4fda86d1604f9a59e532a0a1e974e0d07565c150
triplus <triplus@users.noreply.github.com>
2020-06-19 15:22:37 +0200

    Deterministic compilation and backwards compatibility

--- Travis/travis.sh
+++ Travis/travis.sh
@@ -24,7 +24,9 @@ cd Themes
 for f in *; do
   if [ -d ${f} ]; then
     cd $f
-    /usr/bin/rcc --binary theme.qrc -o ../../deploy/$f.rcc
+    # Deterministic compilation
+    # Backwards compatibility (Qt 5.8 and earlier)
+    /usr/bin/rcc --binary theme.qrc -o ../../deploy/$f.rcc --format-version 1
     cd ..
   fi
 done
